d0e19ddc71ad25387046e5c600b992c052d9f136
Adepsainaveen <adepsainaveen6@gmail.com>
2022-11-03 15:54:11 +0530

    resloved changes

---
+++ Word_Dictionary/WordDictionary.py
@@ -0,0 +1,27 @@
+# have a python dictionary that has a key/value pair that represents a word, and it's definition
+# collect input from the user, input is a word
+# check if the word is in the dictionary
+# print the definition
+
+
+from numpy import pyDictinory
+
+dictionary = pyDictionary("eyes", "indentation", "head")
+
+print(dictionary.getMeanings())
+
+
+"""def main():
+    word_dictonary = {
+        'hi': 'A way of greeting',
+        'eyes': 'An organ for seeing',
+        'earth': 'a planet in space',
+    }
+
+    while True:
+        word = input("Enter a word: ")
+        if word == "":
+            break
+        if word in word_dictonary:
+            print(word, ":", word_dictonary[word])
+main()"""
